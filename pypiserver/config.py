2ea51b565b6f673f55b85ecd894b425f696fe2c2
doyoung kim <doyoung.kim@desilo.ai>
2025-03-14 01:01:07 +0000

    Update config

--- pypiserver/config.py
+++ pypiserver/config.py
@@ -416,12 +416,17 @@ def get_parser() -> argparse.ArgumentParser:
     )
     run_parser.add_argument(
         "-r",
-        "--read-only",
-        metavar="READ_ONLY_FILE",
+        "--read-only-passwords",
+        metavar="READ_ONLY_PASSWORD_FILE",
         help=(
-            "Use an apache htpasswd file READ_ONLY_FILE to set usernames and "
-            "passwords for read-only access. This is useful for allowing "
-            "authenticated users to download packages, but not upload them."
+            "Use an apache htpasswd file READ_ONLY_PASSWORD_FILE to set "
+            "usernames and passwords for read-only access."
+            "\n\n"
+            "This is useful for allowing authenticated users to download "
+            "packages, but not upload them."
+            "\n\n"
+            "Note that read-only users are also need to be added to the "
+            "password file for general authentication."
         ),
     )
     run_parser.add_argument(
@@ -723,7 +728,7 @@ class RunConfig(_ConfigCommon):
         host: str,
         authenticate: t.List[str],
         password_file: t.Optional[str],
-        read_only_file: t.Optional[str],
+        read_only_password_file: t.Optional[str],
         disable_fallback: bool,
         fallback_url: str,
         health_endpoint: str,
@@ -743,7 +748,7 @@ class RunConfig(_ConfigCommon):
         self.host = host
         self.authenticate = authenticate
         self.password_file = password_file
-        self.read_only_file = read_only_file
+        self.read_only_password_file = read_only_password_file
         self.disable_fallback = disable_fallback
         self.fallback_url = fallback_url
         self.health_endpoint = health_endpoint
@@ -770,7 +775,7 @@ class RunConfig(_ConfigCommon):
             "host": namespace.host,
             "authenticate": namespace.authenticate,
             "password_file": namespace.passwords,
-            "read_only_file": namespace.read_only,
+            "read_only_password_file": namespace.read_only_passwords,
             "disable_fallback": namespace.disable_fallback,
             "fallback_url": namespace.fallback_url,
             "health_endpoint": namespace.health_endpoint,
@@ -826,7 +831,7 @@ class RunConfig(_ConfigCommon):
         return auther
 
     def get_read_only(self, auther: t.Optional[t.Callable[[str, str], bool]]) -> t.Callable[[str, str], bool]:
-        if self.read_only_file is None:
+        if self.read_only_password_file is None:
             return lambda _uname, _pw: False
 
         if HtpasswdFile is None:
@@ -837,11 +842,11 @@ class RunConfig(_ConfigCommon):
                 "authentication"
             )
 
-        loaded_read_only_file = HtpasswdFile(self.read_only_file)
+        loaded_read_only_password_file = HtpasswdFile(self.read_only_password_file)
 
         def read_only(uname: str, pw: str) -> bool:
-            loaded_read_only_file.load_if_changed()
-            return loaded_read_only_file.check_password(uname, pw)
+            loaded_read_only_password_file.load_if_changed()
+            return loaded_read_only_password_file.check_password(uname, pw)
 
         return read_only
 
